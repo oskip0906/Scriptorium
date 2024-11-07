4ce20f1c2b77a7aeb0f792b0b48afaf5d5778069
Albert Huynh <itsalbert.huynh@mail.utoronto.ca>
2024-11-07 00:06:28 -0500

    test

--- PP2/pages/Runner/Terminal.tsx
+++ PP2/pages/Runner/Terminal.tsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Editor, { DiffEditor, useMonaco, loader } from '@monaco-editor/react';
 
 
 
+interface TerminalProps {
+  language: string;
+}
 
 
-function Terminal() {
+
+const Terminal: React.FC<TerminalProps> = ({ language }) => {
+
+  const editorRef = useRef(null);
+
+  function onMount(editor: any) {
+    editorRef.current = editor;
+  }
+
+
+  function handleEditorChange(value:any) {
+    // console.log('here is the current model value:', value);
+  }
+
+    function handleEditorValidation(markers:any) {
+      console.log('onValidate:', markers);
+  }
+
+
   return (
     <div>
-        <Editor height="90vh" defaultLanguage="javascript" defaultValue="// some> code" />
+        <Editor height="90vh" 
+        defaultLanguage="javascript" 
+        defaultValue="// some> code"
+        onMount={onMount}
+      onChange={handleEditorChange}
+      onValidate={handleEditorValidation}
+
+        />
     </div>
   )
 }
