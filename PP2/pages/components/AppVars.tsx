f3f24e5511e5dd20c81d22ce311bcb78e801a434
Albert Huynh <itsalbert.huynh@mail.utoronto.ca>
2024-11-18 00:40:54 -0500

    started auth

--- PP2/pages/components/AppVars.tsx
+++ PP2/pages/components/AppVars.tsx
@@ -3,24 +3,19 @@ import React, { createContext, useState, ReactNode, useEffect } from 'react';
 interface GlobalContextType {
   theme: string;
   setTheme: React.Dispatch<React.SetStateAction<string>>;
-<<<<<<< Updated upstream
-=======
+
   userID: string | null;
   setUserID: React.Dispatch<React.SetStateAction<string | null>>;
   admin: string | null;
-  setAdmin: React.Dispatch<React.SetStateAction<string | null>>;
->>>>>>> Stashed changes
+  setAdmin: React.Dispatch<React.SetStateAction<string | null>>
 }
 
 export const AppContext = createContext<GlobalContextType | undefined>(undefined);
 
 export const AppProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [theme, setTheme] = useState('light');
-<<<<<<< Updated upstream
-=======
   const [userID, setUserID] = useState<string | null>(null);
   const [admin, setAdmin] = useState<string | null>("none");
->>>>>>> Stashed changes
 
   useEffect(() => {
     const theme = localStorage.getItem('theme') ?? 'light';
@@ -33,11 +28,7 @@ export const AppProvider: React.FC<{ children: ReactNode }> = ({ children }) => 
 
 
   return (
-<<<<<<< Updated upstream
-    <AppContext.Provider value={{ theme, setTheme }}>
-=======
     <AppContext.Provider value={{ theme, setTheme, userID, setUserID, admin, setAdmin }}>
->>>>>>> Stashed changes
       {children}
     </AppContext.Provider>
   );
