eef89a97f286b94a29c8e68fb9a982fe0973a88d
oskip0906 <oscar.pang050906@gmail.com>
2024-11-27 13:52:41 -0500

    final fix

--- PP2/pages/Admin/index.tsx
+++ PP2/pages/Admin/index.tsx
@@ -150,7 +150,7 @@ const Index: React.FC = () => {
                       className="border p-4 rounded-lg flex items-center justify-between"
                     >
                       <h3 className="text-lg font-medium">
-                        BlogId: {comment.blogPostId} CommentID: {comment.commentId}, Reports: {comment.count}
+                        BlogID: {comment.blogPostId}, CommentID: {comment.commentId}, Reports: {comment.count}
                       </h3>
                       <button
                         onClick={() =>
